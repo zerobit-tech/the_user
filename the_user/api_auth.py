37d8958f45dfa4e5c7251b3e4b2f1b2636cc3463
Zerobit dev <support@zerobit.tech>
2022-10-04 22:47:47 -0500

    auth methods

--- the_user/api_auth.py
+++ the_user/api_auth.py
@@ -8,13 +8,11 @@ from .decorators import auth_methods
 
 class BearerAuth(HttpBearer):
     def authenticate(self, request, token):
-        passed =False
         autherized = None
         for auth_method in auth_methods:
             # call each method if any return not None --> authoerized
             autherized = auth_method(request, token)
             if autherized:
-                passed = True
                 break
-        return autherized if passed else None # unauthoerized 
+        return autherized  # unauthoerized 
  
