1ee52ef2ecd7e1bc2e89df56e1d059bb0b478896
Zerobit dev <support@zerobit.tech>
2022-10-05 09:41:13 -0500

    django 4

--- sample_project/sample_project/settings.py
+++ sample_project/sample_project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django
+from django.utils.encoding import force_str
+django.utils.encoding.force_text = force_str
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
