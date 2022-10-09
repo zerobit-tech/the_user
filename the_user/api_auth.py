2c730f68f2db040d6e1fff5b0a1a97e50e5cdbf2
Zerobit dev <support@zerobit.tech>
2022-10-09 13:09:22 -0500

    internal api

--- the_user/api_auth.py
+++ the_user/api_auth.py
@@ -1,16 +1,21 @@
 
 from webbrowser import get
 from ninja.security import HttpBearer
-from .decorators import _auth_methods
+import logging
+logger = logging.getLogger('ilogger')
 
 
 class BearerAuth(HttpBearer):
     def authenticate(self, request, token):
+        from .decorators import _auth_methods
         autherized = None
         for auth_method in _auth_methods:
             # call each method if any return not None --> authoerized
             autherized = auth_method(request, token)
+            logger.debug(f"auth method {auth_method} : {autherized}")
+
             if autherized:
                 break
+        logger.debug(f"BearerAuth final response: {autherized}")     
         return autherized  # unauthoerized 
  
