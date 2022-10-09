3c4f58116a52ec8b683410d2361a84ae6c3a32a4
Zerobit dev <support@zerobit.tech>
2022-10-09 13:43:02 -0500

    log

--- the_user/api_auth.py
+++ the_user/api_auth.py
@@ -11,7 +11,15 @@ class BearerAuth(HttpBearer):
     def authenticate(self, request, token):
         logger.debug(f"Starting auth")
         from .decorators import _auth_methods
+
         autherized = None
+
+        # if no _auth_methods--> authoerized
+        if not _auth_methods:
+            return token
+
+            
+        # if  _auth_methods--> one must sat that token is authoerized
         for auth_method in _auth_methods:
             # call each method if any return not None --> authoerized
             autherized = auth_method(request, token)
@@ -20,5 +28,7 @@ class BearerAuth(HttpBearer):
             if autherized:
                 break
         logger.debug(f"BearerAuth final response: {autherized}")     
+
+
         return autherized  # unauthoerized 
  
